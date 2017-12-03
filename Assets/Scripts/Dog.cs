e7c990cc339a8c90fe45f77051b5e6400acfb984
Sam Morris <spamcan00@gmail.com>
2017-12-03 18:54:08 +0000

    Dog AI (some of it, anyway). (also the file got tabified by Visual Studio)

--- Assets/Scripts/Dog.cs
+++ Assets/Scripts/Dog.cs
@@ -5,110 +5,129 @@ using UnityEngine;
 [System.Serializable]
 public class Dog : MonoBehaviour
 {
-    [Header("Settings")]
-    public bool isActuallyAPerson = true;
-    public AudioClip m_barkSound;
-    public float m_speed;
-    public float m_attentionSpan;
-    public float m_alertRadius;
-    public string m_name;
-    float offset = 0;
-    DogState m_state = DogState.following;
+	[Header("Settings")]
+	public bool isActuallyAPerson = true;
+	public AudioClip m_barkSound;
+	public float m_speed;
+	public float m_RunSpeed = 500f;
+	public float m_attentionSpan;
+	public float m_alertRadius;
+	public string m_name;
+	float offset = 0;
+	public DogState m_state = DogState.following;
 
-    enum DogState
-    {
-        waiting,
-        following,
-        running,
-        snoofing
-    }
+	public enum DogState
+	{
+		waiting,
+		following,
+		running,
+		snoofing
+	}
 
-    [Header("References")]
-    public LineRenderer m_lead;
-    public Rigidbody2D m_rigidbody;
-    public DistanceJoint2D m_joint;
-    public PointMaker m_route;
-    public GameObject m_owner;
+	[Header("References")]
+	public LineRenderer m_lead;
+	public Rigidbody2D m_rigidbody;
+	public DistanceJoint2D m_joint;
+	public PointMaker m_route;
+	public GameObject m_owner;
 
-    //airhorns
-    [SerializeField]
-    Transform targetthing;
+	//airhorns
+	[SerializeField]
+	Transform targetthing;
 
-    void Awake()
-    {
-        DontDestroyOnLoad(gameObject);
-        if (m_owner)
-        {
-            m_joint = m_owner.AddComponent<DistanceJoint2D>();
-            m_joint.maxDistanceOnly = true;
-            m_joint.autoConfigureDistance = false;
-            m_joint.distance = Player.m_leadLength;
-            m_joint.connectedBody = m_rigidbody;
+	void Awake()
+	{
+		DontDestroyOnLoad(gameObject);
+		if (m_owner)
+		{
+			m_joint = m_owner.AddComponent<DistanceJoint2D>();
+			m_joint.maxDistanceOnly = true;
+			m_joint.autoConfigureDistance = false;
+			m_joint.distance = Player.m_leadLength;
+			m_joint.connectedBody = m_rigidbody;
 
-            m_attentionSpan = Random.Range(1.5f, 15);
+			m_attentionSpan = Random.Range(1.5f, 15);
 
-            float r = Random.Range(0.75f, 1.5f);
-            transform.localScale = Vector3.one * r;
-            m_rigidbody.mass *= r;
-            m_speed *= r;
-            {
-                offset = Random.value * 6;
-            }
-        }
-    }
+			float r = Random.Range(0.75f, 1.5f);
+			transform.localScale = Vector3.one * r;
+			m_rigidbody.mass *= r;
+			m_speed *= r;
+			{
+				offset = Random.value * 6;
+			}
+		}
+	}
 
-    void Update()
-    {
-        if (GameStateManager.instance.m_currentState == GameStateManager.GameSate.Gameplay)
-        {
-            if (!isActuallyAPerson)
-            {
-                if (m_owner.tag == "Player")
-                    m_joint.distance = Player.m_leadLength;
-                else
-                    m_joint.distance = 2.5f;
-                Vector3[] newPos =
-                    {
-                        transform.position,
-                        (transform.position + m_owner.transform.position) / 2,
-                        m_owner.transform.position
-                    };
-                m_lead.SetPositions(newPos);
-            }
-            Movement();
-        }
+	void Update()
+	{
+		if (GameStateManager.instance.m_currentState == GameStateManager.GameSate.Gameplay)
+		{
+			if (!isActuallyAPerson)
+			{
+				if (m_owner.tag == "Player")
+					m_joint.distance = Player.m_leadLength;
+				else
+					m_joint.distance = 2.5f;
+				Vector3[] newPos =
+					{
+						transform.position,
+						(transform.position + m_owner.transform.position) / 2,
+						m_owner.transform.position
+					};
+				m_lead.SetPositions(newPos);
+			}
+			Movement();
+		}
 
 
-    }
+	}
 
-    Vector3 targetPos;
+	Vector3 targetPos;
+	public float m_fSnoofRadius = 2.0f;
+	float m_fNextSnoofTime = -1;
+	float m_fSnoofTimeMin = 1f;
+	float m_fSnoofTimeMax = 6f;
+	Vector3 m_SnoofTarget;
 
-    void Movement()
-    {
-        Vector3 target = Vector3.zero;
+	void Movement()
+	{
+		Vector3 target = Vector3.zero;
+		float fSpeed = m_speed;
 
-        switch (m_state)
-        {
-            case DogState.waiting:
-                break;
-            case DogState.following:
-                target = m_route.WhereGo(transform.position, offset, !isActuallyAPerson);
-                break;
-            case DogState.running:
-                break;
-            case DogState.snoofing:
-                break;
-        }
+		switch (m_state)
+		{
+			case DogState.waiting:
+				break;
+			case DogState.following:
+				target = m_route.WhereGo(transform.position, offset, !isActuallyAPerson);
+				break;
+			case DogState.running:
+				// Currently just the same as following, but faster.
+				target = m_route.WhereGo(transform.position, offset, !isActuallyAPerson);
+				fSpeed = m_RunSpeed;
+				break;
+			case DogState.snoofing:
+				// Keep on heading towards a snoof location until we've either gotten to it, or we've gone for a certain amount of time.
+				// TODO: Allow the player to pull the pup away from his snoof target and cancel out of snoof mode.
+				if (m_fNextSnoofTime <= Time.time || Vector3.Distance(target, transform.position) <= 0.5f) {
+					Vector3 newTarget = (Random.insideUnitCircle * m_fSnoofRadius);
+					m_SnoofTarget = m_route.WhereGo(transform.position, offset, !isActuallyAPerson) + newTarget;
+					m_fNextSnoofTime = Random.Range(m_fSnoofTimeMin, m_fSnoofTimeMax) + Time.time;
+				}
 
-        targetthing.transform.position = target - Vector3.back * 10;
+				target = m_SnoofTarget;
+				break;
+		}
 
-        float n = .0f;
-        Vector3 noise = new Vector3(Random.Range(-n, n), Random.Range(-n, n), Random.Range(-n, n));
-        m_rigidbody.AddForce((target - transform.position + noise).normalized * m_speed);
+		targetthing.transform.position = target - Vector3.back * 10;
 
-        Vector3 diff = target - transform.position + noise;
-        diff.Normalize();
-        float z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.LerpUnclamped(transform.rotation, Quaternion.Euler(0, 0, z + 180), Time.deltaTime);
-    }
+		float n = .0f;
+		Vector3 noise = new Vector3(Random.Range(-n, n), Random.Range(-n, n), Random.Range(-n, n));
+		m_rigidbody.AddForce((target - transform.position + noise).normalized * fSpeed);
+
+		Vector3 diff = target - transform.position + noise;
+		diff.Normalize();
+		float z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.LerpUnclamped(transform.rotation, Quaternion.Euler(0, 0, z + 180), Time.deltaTime);
+	}
 }
